8865784330b6c29f09b1f78f5042b44ee1bfd108
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-10-19 08:37:17 -0700

    #173. Styling Summary Area

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
@@ -8,11 +9,18 @@ export const ExpensesSummary = (props) => {
   const { expenses } = props;
   const expensesCount = expenses.length;
   const expensesTotal = numeral(selectExpensesTotal(expenses) / 100).format('$0,0.00');
-  const adjective = expensesCount === 1 ? 'expense' : 'expenses';
+  const expensesWord = expensesCount === 1 ? 'expense' : 'expenses';
 
   return (
-    <div>
-      <h1>{`Viewing ${expensesCount} ${adjective} totaling ${expensesTotal}`}</h1>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">
+          Viewing <span>{expensesCount}</span> {expensesWord} totaling <span>{expensesTotal}</span>
+        </h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/add">Add Expense</Link>
+        </div>
+      </div>
     </div>
   )
 }
